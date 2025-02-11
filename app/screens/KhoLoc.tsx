f9ef72d2da300b80427addcccd8c7ca0050061db
NguyenHuuTrong2707 <nguyenhuutrongvtabs@gmail.com>
2025-02-11 14:59:19 +0700

    update kho lộc

--- app/screens/KhoLoc.tsx
+++ app/screens/KhoLoc.tsx
@@ -1,12 +1,117 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import BottomTabNavigator from '../navigation/BottomNavigation';
+import { FlatList, ImageBackground, Text, View } from 'react-native';
+import { styles } from './styles/KhoLocStyle'
+import { collection, doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { app } from "../../firebase/firebaseConfig";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image } from 'react-native';
+import { useAuth } from "@/contexts/AuthContext";
+interface Page_KhoLoc {
+    imgBackGround: string
+    imgFrame: string
+    title: string
+    logo: string
+}
+interface Gift {
+    kho_loc_id: string;
+    name: string;
+    imgQua: string;
+}
+const db = getFirestore(app);
 const Page_KhoLoc: React.FC = () => {
+    // load firebase
+    const [page_KhoLoc, setPage_KhoLoc] = useState<Page_KhoLoc | null>(null);
+    const { user } = useAuth();
+    const [userGifts, setUserGifts] = useState<Gift[]>([])
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            collection(db, "KhoLoc"),
+            (querySnapshot) => {
+                if (querySnapshot.empty) {
+                    console.log("No documents found in 'KhoLoc' collection.");
+                    return;
+                }
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data();
+                    setPage_KhoLoc({
+                        imgBackGround: data.imgBackGround,
+                        imgFrame: data.imgFrame,
+                        title: data.title,
+                        logo: data.logo
+
+                    });
+                });
+            },
+            (error) => {
+                console.log("Error fetching document:", error);
+            }
+        );
+        return () => unsubscribe();
+    }, []);
+    //load kho lộc của user
+    useEffect(() => {
+        if (!user) return;
+        const userRef = doc(db, "users", user.uid);
+        const unsubscribe = onSnapshot(userRef, (docSnapshot) => {
+            if (docSnapshot.exists()) {
+                const data = docSnapshot.data();
+                setUserGifts(data.khoLoc || []);
+            }
+        });
+
+        return () => unsubscribe();
+    }, [user]);
     return (
-        <View>
-            <Text>Kho lộc</Text>
-           
-        </View>
+        <ImageBackground
+            source={{ uri: page_KhoLoc?.imgBackGround }}
+            resizeMode='cover'
+            style={styles.imgBackGround}
+        >
+            <SafeAreaView style={styles.container}>
+                {/* Title */}
+                <Text style={styles.title}>{page_KhoLoc?.title}</Text>
+                {/* Logo */}
+                <View style={styles.logoContainer}>
+                    <Image source={{ uri: page_KhoLoc?.logo }}
+                        style={styles.logo}
+                    ></Image>
+                </View>
+                {/* Frame */}
+                <ImageBackground
+                    source={{ uri: page_KhoLoc?.imgFrame }}
+                    resizeMode='cover'
+                    style={styles.frame}
+                >
+                    <FlatList
+                        data={userGifts}
+                        keyExtractor={(item) => item.kho_loc_id}
+                        numColumns={3}
+                        contentContainerStyle={{
+                            paddingVertical: 10,
+                            paddingHorizontal: 15,
+                            flexGrow: 1,
+                        }}
+                        columnWrapperStyle={{ gap: 10 }}
+                        showsVerticalScrollIndicator={false}
+                        renderItem={({ item }) => (
+                            <View style={styles.giftContainer}>
+                                <ImageBackground
+                                    source={require('../../assets/images/quabg.png')}
+                                    style={styles.quabg}
+                                >
+                                    <Image source={{ uri: item.imgQua }} style={styles.giftImage} />
+                                    <View style={styles.giftInfo}>
+                                        <Text style={styles.giftName}>{item.name}</Text>
+                                    </View>
+                                </ImageBackground>
+                            </View>
+
+                        )}
+                    />
+                </ImageBackground>
+            </SafeAreaView>
+        </ImageBackground>
+
     )
 }
 
---
+++ app/screens/styles/KhoLocStyle.ts
@@ -0,0 +1,56 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    imgBackGround: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        marginTop: 40,
+        color: '#FAD93C',
+        lineHeight: 33,
+        fontFamily: 'SVN-Cookie'
+    },
+    logoContainer: {
+        paddingVertical: 10
+    },
+    logo: {
+        width: 104,
+        height: 31,
+    },
+    frame: {
+        width: 335,
+        height: 511,
+    },
+    quabg: {
+        width: 96,
+        height: 155,
+        alignItems: 'center',
+        padding: 5,
+      
+    },
+    giftContainer: {
+        marginBottom: 15,  
+    },
+    giftImage: {
+        width: 65,
+        height: 65,
+        resizeMode: 'contain'
+    },
+    giftInfo: {
+        flex: 1,
+    },
+    giftName: {
+        fontSize: 10,
+        fontWeight: 700,
+        color: "#732F2F",
+        textAlign:'center',
+        lineHeight: 13,
+    },
+
+});
